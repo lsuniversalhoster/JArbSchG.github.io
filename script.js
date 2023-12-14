93e1f861b16d69d6019cfeb046e17e9b49e25a75
lsuniversalhoster <93405279+lsuniversalhoster@users.noreply.github.com>
2023-12-14 14:41:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+// Intro-Effekt
+window.addEventListener('load', () => {
+  const intro = document.querySelector('.intro');
+  const introText = document.getElementById('intro-text');
+  const bodyBlur = document.querySelector('body::before');
+
+  setTimeout(() => {
+    introText.style.transform = 'scale(1)';
+  }, 500);
+
+  setTimeout(() => {
+    bodyBlur.style.filter = 'blur(0)';
+    intro.style.opacity = '0';
+    setTimeout(() => {
+      intro.style.display = 'none';
+    }, 1000);
+  }, 2500);
+});
+
+// Aufbaueffekt für die Abschnitte
 function revealSections() {
   const sections = document.querySelectorAll('section');
   sections.forEach((section, index) => {
