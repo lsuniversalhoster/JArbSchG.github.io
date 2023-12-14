c3f9f8964190e517e11ebd442beb685074505ac5
lsuniversalhoster <93405279+lsuniversalhoster@users.noreply.github.com>
2023-12-14 15:32:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-function revealSections() {
+function revealSectionsSmooth() {
   const sections = document.querySelectorAll('section');
-  sections.forEach((section, index) => {
+  sections.forEach(section => {
     section.style.opacity = '0';
+    section.style.transform = 'scale(0.9)';
+  });
+
+  let delay = 0;
+  sections.forEach(section => {
     setTimeout(() => {
       section.style.opacity = '1';
-    }, 500 * index);
+      section.style.transform = 'scale(1)';
+    }, delay);
+    delay += 100;
   });
 }
 
-window.addEventListener('load', revealSections);
+window.addEventListener('load', revealSectionsSmooth);
