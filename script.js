21ae7c5004d052f5a9c95a23b25b4ae14829c5c0
lsuniversalhoster <93405279+lsuniversalhoster@users.noreply.github.com>
2023-12-14 10:29:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+// JS Animations
+const sections = document.querySelectorAll('section');
 
+const options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.2
+};
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('appear');
+      observer.unobserve(entry.target);
+    }
+  });
+}, options);
+
+sections.forEach(section => {
+  observer.observe(section);
+});
