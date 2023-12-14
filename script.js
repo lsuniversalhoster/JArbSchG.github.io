3391f3a93273bf92f75dbabbda523d833c634d79
lsuniversalhoster <93405279+lsuniversalhoster@users.noreply.github.com>
2023-12-14 15:39:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
-function revealSectionsSmooth() {
+function revealSectionsSmoothly() {
   const sections = document.querySelectorAll('section');
-  sections.forEach(section => {
-    section.style.opacity = '0';
-    section.style.transform = 'scale(0.9)';
-  });
 
-  let delay = 0;
-  sections.forEach(section => {
+  sections.forEach((section, index) => {
+    const topPosition = section.getBoundingClientRect().top + window.scrollY;
+    const screenHeight = window.innerHeight;
+
+    if (topPosition < screenHeight) {
+      section.style.opacity = '0';
+      section.style.transform = 'translateY(50px)';
+    } else {
+      section.style.opacity = '0';
+      section.style.transform = 'translateY(100px)';
+    }
+
+    section.style.transition = 'opacity 1s ease, transform 1s ease';
+
     setTimeout(() => {
       section.style.opacity = '1';
-      section.style.transform = 'scale(1)';
-    }, delay);
-    delay += 100;
+      section.style.transform = 'none';
+    }, 500 * index);
   });
 }
 
-window.addEventListener('load', revealSectionsSmooth);
+window.addEventListener('load', revealSectionsSmoothly);
+window.addEventListener('scroll', revealSectionsSmoothly);
